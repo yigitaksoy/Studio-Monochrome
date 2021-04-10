d75b67e9784b3c50fd189df2f791f8204428687c
yigitaksoy <yigitaksoy@gmail.com>
2021-04-10 21:52:00 +0200

    Update overlay navigation animation speed

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,11 +190,11 @@ textRight.forEach((item, index) => {
  
  
  var navigation = new TimelineLite({paused:true, reversed:true});
- navigation.to("#navigationWrap", 0.5, {opacity: 1, display: 'block'})
+ navigation.to("#navigationWrap", 0.4, {opacity: 1, display: 'block'})
            .to(".navbar", 0.3, {opacity: 0}, "-=0.1")
-           .to(".close", 0.3, {display: "block", opacity: 1}, "-=0.1")
-           .from(".menu", 0.5, {opacity: 0, y: 30})
-           .from(".social", 0.5, {opacity: 0});
+           .to(".close", 0.1, {display: "block", opacity: 1}, "-=0.1")
+           .from(".menu", 0.3, {opacity: 0, y: 30})
+           .from(".social", 0.4, {opacity: 0});
  
  $(".navbar, .close").click (function() {
    navigation.reversed() ? navigation.play() : navigation.reverse();
